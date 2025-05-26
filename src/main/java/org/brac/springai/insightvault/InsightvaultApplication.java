3027856f0fd38bdb6f98d7114b60698a3a31df9e
Md. Toyeb <md.toyeb@bracits.com>
2025-05-26 10:40:33 +0600

    initial commit

---
+++ src/main/java/org/brac/springai/insightvault/InsightvaultApplication.java
@@ -0,0 +1,13 @@
+package org.brac.springai.insightvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightvaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightvaultApplication.class, args);
+    }
+
+}
