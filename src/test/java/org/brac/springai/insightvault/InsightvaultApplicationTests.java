c0b6c9a69b102183fbf2c74f4afcae08e0756903
Md. Toyeb <md.toyeb@bracits.com>
2025-05-26 10:40:33 +0600

    initial commit

---
+++ src/test/java/org/brac/springai/insightvault/InsightvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brac.springai.insightvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightvaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
